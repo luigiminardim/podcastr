97098ba8e86bd2eac05eb6b53fc1c3800107a053
Luigi Minardi <luigiminardim@gmail.com>
2021-04-21 15:19:14 -0300

    Add json server.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import { GetStaticProps } from "next";
 import Header from "../components/shared/Header";
 
-export default function HomePage() {
-  return <h1>Index</h1>;
+export default function HomePage({ episodes }) {
+  return <>{JSON.stringify(episodes)}</>;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+  const _8hours = 8 * 60 * 60;
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: _8hours,
+  };
+};
